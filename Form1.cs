38357b8c6ffb90dc9aa1564fa114777ca4c3013a
Tiara Peña <148275423+Tiaraxx@users.noreply.github.com>
2024-11-08 15:04:43 -0400

    Arreglando codigos

--- Form1.cs
+++ Form1.cs
@@ -13,50 +13,49 @@ namespace cronometro
 {
     public partial class Form1 : Form
     {
-        private int tiempoSegundos = 0; // Tiempo en segundos
+        private int tiempoSegundos = 0; 
 
-        // Constructor
         public Form1()
         {
             InitializeComponent();
         }
 
-        // Evento que se ejecuta cuando el formulario carga
+      
         private void Form1_Load(object sender, EventArgs e)
         {
-            // Puedes agregar lógica aquí si es necesario cuando el formulario se carga
+    
         }
 
-        // Evento para detener el cronómetro
+       
         private void button2_Click(object sender, EventArgs e)
         {
-            timer1.Stop(); // Detiene el Timer
-            button1.Enabled = true; // Activa el botón de "Iniciar"
-            button2.Enabled = false; // Desactiva el botón de "Detener"
+            timer1.Stop(); 
+            button1.Enabled = true; 
+            button2.Enabled = false; 
         }
 
-        // Evento para iniciar el cronómetro
+   
         private void button1_Click(object sender, EventArgs e)
         {
-            timer1.Start(); // Inicia el Timer
-            button1.Enabled = false; // Desactiva el botón de "Iniciar" cuando está en marcha
-            button2.Enabled = true; // Activa el botón de "Detener"
+            timer1.Start(); 
+            button1.Enabled = false;
+            button2.Enabled = true; 
         }
 
-        // Evento que se ejecuta cada vez que el Timer se actualiza (cada segundo)
+        
         private void timer1_Tick(object sender, EventArgs e)
         {
-            tiempoSegundos++;  // Incrementa el tiempo cada segundo
-            label1.Text = TimeSpan.FromSeconds(tiempoSegundos).ToString(@"hh\:mm\:ss"); // Muestra el tiempo en formato HH:MM:SS
+            tiempoSegundos++;  
+            label1.Text = TimeSpan.FromSeconds(tiempoSegundos).ToString(@"hh\:mm\:ss"); 
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            timer1.Stop(); // Detiene el Timer
-            tiempoSegundos = 0; // Reinicia el contador de tiempo
-            label1.Text = "00:00:00"; // Reinicia el texto del Label
-            button1.Enabled = true; // Activa el botón de "Iniciar"
-            button2.Enabled = false; // Desactiva el botón de "Detener"
+            timer1.Stop(); 
+            tiempoSegundos = 0; 
+            label1.Text = "00:00:00";
+            button1.Enabled = true; 
+            button2.Enabled = false; 
         }
 
         private void label1_Click(object sender, EventArgs e)
